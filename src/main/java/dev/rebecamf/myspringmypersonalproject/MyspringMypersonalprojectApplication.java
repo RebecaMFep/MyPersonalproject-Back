7a0fea54b15c0e3204712af0ef5bf4e51b88e3d6
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-11 19:41:29 +0100

    init repo

---
+++ src/main/java/dev/rebecamf/myspringmypersonalproject/MyspringMypersonalprojectApplication.java
@@ -0,0 +1,13 @@
+package dev.rebecamf.myspringmypersonalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringMypersonalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringMypersonalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rebecamf/myspringmypersonalproject/MyspringMypersonalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rebecamf.myspringmypersonalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringMypersonalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
