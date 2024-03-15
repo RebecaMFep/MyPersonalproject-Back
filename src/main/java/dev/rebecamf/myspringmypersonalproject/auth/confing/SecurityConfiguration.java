ee157393547e18aaf27e3824d964327bff1f7266
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-15 20:31:55 +0100

    added security configuration

---
+++ src/main/java/dev/rebecamf/myspringmypersonalproject/auth/confing/SecurityConfiguration.java
@@ -0,0 +1,79 @@
+package dev.rebecamf.myspringmypersonalproject.auth.confing;
+
+
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import dev.rebecamf.myspringmypersonalproject.security.JpaUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Value("${api-endpoint}")
+    String endpoint;
+
+    JpaUserDetailsService jpaUserDetailsService;
+
+    public SecurityConfiguration(JpaUserDetailsService jpaUserDetailsService) {
+        this.jpaUserDetailsService = jpaUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .logout(out -> out
+                        .logoutUrl(endpoint + "/logout")
+                        .deleteCookies("JSESSIONID"))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, endpoint + "/activities", endpoint + "/images/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, endpoint + "/activities", endpoint + "/images").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, endpoint + "/activities/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, endpoint + "/activities/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, endpoint + "/users/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, endpoint + "/login").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers(HttpMethod.GET, endpoint + "/activities/{id}/subscription").hasAnyRole("USER")
+                        .anyRequest().authenticated())
+                .userDetailsService(jpaUserDetailsService)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED));
+
+        http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
