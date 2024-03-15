c9a2e0e38656b45a1ba7570a36f702064e2579d6
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-15 21:55:48 +0100

    created role file

---
+++ src/main/java/dev/rebecamf/myspringmypersonalproject/roles/RoleException.java
@@ -0,0 +1,13 @@
+package dev.rebecamf.myspringmypersonalproject.roles;
+
+public class RoleException extends RuntimeException {
+    
+    public RoleException(String message) {
+        super(message);
+    }
+
+    public RoleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
