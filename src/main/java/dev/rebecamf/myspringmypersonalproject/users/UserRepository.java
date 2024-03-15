e12f9b3fd68304e5264d536bd5bcc0ada24a6564
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-15 21:47:17 +0100

    created users file

---
+++ src/main/java/dev/rebecamf/myspringmypersonalproject/users/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.rebecamf.myspringmypersonalproject.users;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    
+    public Optional<User> findByUsername(String username);
+
+}
