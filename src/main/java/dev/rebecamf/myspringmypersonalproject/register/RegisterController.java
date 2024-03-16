78b2c3e6757f0d7671eb8e014a993f13215ff591
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-16 11:51:36 +0100

    created register file

---
+++ src/main/java/dev/rebecamf/myspringmypersonalproject/register/RegisterController.java
@@ -0,0 +1,25 @@
+package dev.rebecamf.myspringmypersonalproject.register;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import dev.rebecamf.myspringmypersonalproject.users.User;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}")
+public class RegisterController {
+
+    RegisterService service;
+
+    public RegisterController(RegisterService service) {
+        this.service = service;
+    }
+
+    @PostMapping(path = "/users/register")
+    public String register(@RequestBody User newUser) {
+        return service.save(newUser);
+    }
+
+}
