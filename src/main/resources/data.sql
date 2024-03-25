060fd70559a84cfedbc4e8692a72c2638b0e13fb
RebecaMFep <martinezfeliperebeca@gmail.com>
2024-03-25 04:03:34 +0100

    fixed data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,24 @@
-INSERT INTO activities(id_activity,title,date,hour,place,description,capacity,is_outstanding,img) VALUES (DEFAULT,"Concierto","01/01/2025","22:00","Madrid","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,true,"event2.jpg");
-INSERT INTO activities(id_activity,title,date,hour,place,description,capacity,is_outstanding,img) VALUES (DEFAULT,"Concierto","01/02/2025","23:00","Madrid","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,true,"event3.jpg");
-INSERT INTO activities(id_activity,title,date,hour,place,description,capacity,is_outstanding,img) VALUES (DEFAULT,"Concierto","01/03/2025","21:00","Madrid","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event6.jpg");
-INSERT INTO activities(id_activity,title,date,hour,place,description,capacity,is_outstanding,img) VALUES (DEFAULT,"Concierto","01/04/2025","22:00","Madrid","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event7.png");
-INSERT INTO acivities(id_activity,title,date,hour,place,description,capacity,is_outstanding,img) VALUES (DEFAULT,"Concierto","01/05/2025","19:00","Madrid","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event8.jpeg");
+INSERT INTO activities(id_activity,name,location,days,month,time,ageRange,capacity,,description) VALUES (DEFAULT,"Escalada","Rocódromo San Agustín","5","1","11:00" "2","10" "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,true,"event2.jpg");
+INSERT INTO activities(id_activity,name,location,days,month,time,ageRange,capacity,,description) VALUES (DEFAULT,"Surf","San Lorenzo","6","1" "9:00","1","10","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,true,"event3.jpg");
+INSERT INTO activities(id_activity,name,location,days,month,time,ageRange,capacity,description) VALUES (DEFAULT,"Volley-Playa","San Lorenzo","2,4","2" "11:00","2","12","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event6.jpg");
+INSERT INTO activities(id_activity,name,location,days,month,time,ageRange,capacity,,description) VALUES (DEFAULT,"Skate","Cimavilla","1,3","1" "10:00","1","10","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event7.png");
+INSERT INTO acivities(id_activity,name,location,days,month,time,ageRange,capacity,description) VALUES (DEFAULT,"Natación","Santa Olaya","3","2" "10:00","2","12","Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempor vulputate dui, id bibendum lectus venenatis quis. Aliquam ac ligula porttitor, scelerisque nisi ac, sollicitudin augue.",100,false,"event8.jpeg");
+
+
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Lunes");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Martes");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Miércoles");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Jueves");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Viernes");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Sábado");
+INSERT INTO days(id_day,name) VALUES (DEFAULT,"Domingo");
+
+
+INSERT INTO months(id_month,name) VALUES (DEFAULT,"Julio");
+INSERT INTO months(id_month,name) VALUES (DEFAULT,"Agosto");
+
+INSERT INTO ageRanges(id_ageRange,name) VALUES (DEFAULT,"3-6 años");
+INSERT INTO ageRanges(id_ageRange,name) VALUES (DEFAULT,"7-12 años");
 
 INSERT INTO roles(id_role,name) VALUES (DEFAULT,"ROLE_ADMIN");
 INSERT INTO roles(id_role,name) VALUES (DEFAULT,"ROLE_USER");
